dad4e52361b72c053e194258c6fce357096a37e1
Aman-62 <aman230630@gmail.com>
2024-05-22 17:22:13 +0530

    Map,Filter, Reduce

--- javascript-6/script.js
+++ javascript-6/script.js
@@ -30,28 +30,28 @@ const inputClosePin = document.querySelector(".form-input-pin");
 const account1 = {
     owner: "Rahul Kumar",
     movements: [200, 450, -400, 3000, -650, -130, 70, 1300, 100],
-    interestRate: 1.2,
+    interestRate: 7,
     pin: 1111,
 };
 
 const account2 = {
     owner: "Vicky Singh",
     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
+    interestRate: 6,
     pin: 2222,
 };
 
 const account3 = {
     owner: "Mohit Sharma",
     movements: [200, -200, 340, -20, 50, 400, -460],
-    interestRate: 0.7,
+    interestRate: 6.7,
     pin: 3333,
 };
 
 const account4 = {
     owner: "Raj Verma",
     movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
+    interestRate: 8,
     pin: 4444,
 };
 
@@ -72,17 +72,46 @@ const displayMovements = (movements) => {
         containerMovements.insertAdjacentHTML("afterbegin", html);
     });
 };
-
 displayMovements(account1.movements);
 
-const user = "Rahul Kumar Singh";
-const username = user
-    .toLowerCase()
-    .split(" ")
-    .map((word) => word[0])
-    .join("");
+const calcDisplayBalance = (movements) => {
+    const balance = movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `${balance}₹`;
+};
+calcDisplayBalance(account1.movements);
 
-console.log(username);
+const calcDisplaySummary = (movements) => {
+    const incomes = movements
+        .filter((mov) => mov > 0)
+        .reduce((acc, mov) => acc + mov, 0);
+
+    const out = movements
+        .filter((mov) => mov < 0)
+        .reduce((acc, mov) => acc + mov, 0);
+
+    const interest = movements
+        .filter((mov) => mov > 0)
+        .map((deposit) => (deposit * 8) / 100)
+        .reduce((acc, mov) => acc + mov, 0);
+
+    labelSumIn.textContent = `${incomes}₹`;
+    labelSumOut.textContent = `${out}₹`;
+    labelSumInterest.textContent = `${interest}₹`;
+};
+calcDisplaySummary(account1.movements);
+
+const createUsernames = (accs) => {
+    accs.forEach(function (acc) {
+        acc.username = acc.owner
+            .toLowerCase()
+            .split(" ")
+            .map((name) => name[0])
+            .join("");
+    });
+};
+createUsernames(accounts);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /*
 
@@ -115,8 +144,6 @@ console.log(letters);
 
 //* JOIN
 console.log(letters.join(" - "));
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 console.log("---- forOf ----");
 for (const [i, movement] of movements.entries()) {
@@ -161,4 +188,69 @@ for (let movement of movementsUSD) {
 }
 console.log(movementsInr2);
 
+
+//* FILTER
+
+const deposit = movements.filter((el, i, arr) => el > 0);
+// console.log(deposit);
+
+const depositFor = [];
+
+for (const mov of movements) if (mov > 0) depositFor.push(mov);
+
+// console.log(depositFor);
+
+//* REDUCE
+const balance = movements.reduce(function (acc, el, i, arr) {
+    console.log(`Iteration ${i}: ${acc}`);
+    return acc + el;
+}, 0);
+
+console.log(balance);
+
+//* 1. Calculate the dog age human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog > 2 years old, humanAge = 16 + dogAge * 4.
+//* 2. Exclude all dogs that are less than 18 human years old( which is same as keeping dogs that are at least 18 years old)
+//* 3. Calculate the average human age of all adult dogs
+//* 4. Run the function for both test dataset.
+
+// TestData 1: [5, 2, 4, 1, 15, 8, 3]
+// TestData 2: [16, 6, 10, 5, 6, 1, 7]
+
+const calcAverageHumanAge = function (ages) {
+    const humanAges = ages.map((age) => {
+        return age <= 2 ? 2 * age : 16 + age * 4;
+    });
+    const adults = humanAges.filter((age) => age >= 18);
+    console.log(humanAges);
+    console.log(adults);
+
+    // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+    const average = adults.reduce(
+        (acc, age, i, arr) => acc + age / arr.length,
+        0
+    );
+
+    // 10, 20, 25, 40, 5
+    // 100 / 5 => 20
+    // 10/5 + 20/5 + 25/5 + 40/5 + 5/5 => 2+4+5+8+1 => 20
+
+    console.log(average);
+    return average;
+};
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+
+
 */
+
+const usdToInr = 83.29;
+
+const totalDepositsInr = movements
+    .filter((mov, i, arr) => mov > 0)
+    .map((mov, i, arr) => {
+        // console.log("Map: ", arr);
+        return mov * usdToInr;
+    })
+    .reduce((acc, mov, i, arr) => acc + mov, 0);
+
+console.log(totalDepositsInr);
