f33b1c4bd81522f00c10d6b77925f880b6ebf856
Aman-62 <aman230630@gmail.com>
2024-06-20 16:05:09 +0530

    dom

--- javascript-7/script.js
+++ javascript-7/script.js
@@ -63,6 +63,25 @@ btnScrollTo.addEventListener("click", (e) => {
 
 //* Page navigation
 
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//     el.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         const id = this.getAttribute("href");
+//         // console.log(id);
+//         document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//     });
+// });
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+    e.preventDefault();
+    // console.log(e.target);
+
+    if (e.target.classList.contains("nav__link")) {
+        const id = e.target.getAttribute("href");
+        document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+});
+
 /*
 ///////////////////////////////
 ///////////////////////////////
@@ -195,6 +214,34 @@ document.querySelector(".nav").addEventListener(
 const h1 = document.querySelector("h1");
 let value = true;
 console.log(value);
+
+//* DOM Traversing
+
+const h1 = document.querySelector("h1");
+
+// Going downwards: child
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "blue";
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = `var(--gradient-secondary)`;
+h1.closest("h1").style.background = `var(--gradient-primary)`;
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
 
 
 */
