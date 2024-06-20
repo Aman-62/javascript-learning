6a9b092aa8c3f3882ede999d159330b5d52c0214
Aman-62 <aman230630@gmail.com>
2024-06-20 17:23:44 +0530

    sticky navbar

--- javascript-7/script.js
+++ javascript-7/script.js
@@ -5,6 +5,7 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
+const nav = document.querySelector(".nav");
 const section1 = document.querySelector("#section--1");
 
 //* Modal window
@@ -79,6 +80,69 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     if (e.target.classList.contains("nav__link")) {
         const id = e.target.getAttribute("href");
         document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+});
+
+//* Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+    const clicked = e.target.closest(".operations__tab");
+
+    if (!clicked) return;
+
+    // console.log(clicked);
+    tabs.forEach((tab) => tab.classList.remove("operations__tab--active"));
+    tabContent.forEach((tc) =>
+        tc.classList.remove("operations__content--active")
+    );
+
+    clicked.classList.add("operations__tab--active");
+
+    // console.log(clicked.dataset.tab);
+
+    document
+        .querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add("operations__content--active");
+});
+
+//* Menu fade animation
+
+const handleHover = function (e, opacity) {
+    if (e.target.classList.contains("nav__link")) {
+        const link = e.target;
+        const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+        const logo = link.closest(".nav").querySelector("img");
+
+        siblings.forEach((el) => {
+            if (el !== link) el.style.opacity = opacity;
+        });
+
+        logo.style.opacity = opacity;
+    }
+};
+
+nav.addEventListener("mouseover", function (e) {
+    handleHover(e, 0.5);
+});
+nav.addEventListener("mouseout", function (e) {
+    handleHover(e, 1);
+});
+
+//* Sticky navigation
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener("scroll", function () {
+    // console.log(window.scrollY);
+
+    if (window.scrollY > initialCoords.top) {
+        nav.classList.add("sticky");
+    } else {
+        nav.classList.remove("sticky");
     }
 });
 
